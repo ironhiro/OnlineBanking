79a04398c2afcd2bcd9dcd25dc1ab2ba78bacdc9
ironhiro <iuncehiro@gmail.com>
2018-12-03 16:25:55 +0900

    add admin model

---
+++ src/Model/Admin.java
@@ -0,0 +1,19 @@
+
+import java.util.*;
+
+public class Admin extends Member {
+
+    public Admin() {
+    }
+
+    public void addNewItem() {
+        // TODO implement here
+        
+    }
+
+    public void notifyNewItem() {
+        // TODO implement here
+        
+    }
+
+}
