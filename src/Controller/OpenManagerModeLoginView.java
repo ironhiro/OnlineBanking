72de065c6fc0430070443c0ec575dc4bfb75d63d
sungHee96 <45479121+sungHee96@users.noreply.github.com>
2018-12-05 21:13:59 +0900

    .

--- src/Controller/OpenManagerModeLoginView.java
+++ src/Controller/OpenManagerModeLoginView.java
@@ -1,5 +1,15 @@
 package Controller;
 
-public class OpenManagerModeLoginView {
+import View.ManagerModeLoginView;
+
+public class OpenManagerModeLoginView implements Controller{
+
+	@Override
+	public void execute() {
+		// TODO Auto-generated method stub
+		ManagerModeLoginView register = new ManagerModeLoginView();
+		register.setLocationRelativeTo(null);
+		register.setVisible(true);
+	}
 
 }
